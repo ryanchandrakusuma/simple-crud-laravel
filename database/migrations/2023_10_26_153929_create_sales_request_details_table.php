035c9a8a3c691074491670abe08dddfe53578ec5
Ryan Chandra Kusuma <rookie3ryan@gmail.com>
2023-10-26 22:59:05 +0700

    migrations : for transactions

---
+++ database/migrations/2023_10_26_153929_create_sales_request_details_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales_request_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('sales_request_id')->references('id')->on('sales_requests')->onDelete('cascade');
+            $table->foreignId('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales_request_details');
+    }
+};
