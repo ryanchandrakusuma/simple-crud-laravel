f05d5a25046b063b1892e6efa8c925ccc50db15f
Ryan Chandra Kusuma <rookie3ryan@gmail.com>
2023-10-26 12:47:30 +0700

    routes : refactor unnecessary routes

--- routes/web.php
+++ routes/web.php
@@ -32,32 +32,6 @@ Route::middleware('auth')->group(function () {
     Route::put('/customers/{id}', [CustomerController::class, 'update'])->name('customers.update');
     Route::delete('/customers/{id}', [CustomerController::class, 'destroy'])->name('customers.destroy');
 
-    //professions
-    Route::get('/professions', [ProfessionController::class, 'index'])->name('professions.index');
-    Route::get('/professions/create', [ProfessionController::class, 'create'])->name('professions.create');
-    Route::get('/professions/{id}/edit', [ProfessionController::class, 'show'])->name('professions.edit');
-    Route::post('/professions/store', [ProfessionController::class, 'store'])->name('professions.store');
-    Route::put('/professions/{id}', [ProfessionController::class, 'update'])->name('professions.update');
-    Route::delete('/professions/{id}', [ProfessionController::class, 'destroy'])->name('professions.destroy');
-
-    //projects
-    Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
-    Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
-    Route::post('/projects/store', [ProjectController::class, 'store'])->name('projects.store');
-    Route::get('/projects/{id}/edit', [ProjectController::class, 'show'])->name('projects.edit');
-    Route::put('/projects/{id}', [ProjectController::class, 'update'])->name('projects.update');
-    Route::delete('/projects/{id}', [ProjectController::class, 'destroy'])->name('projects.destroy');
-
-    //services
-    Route::get('/services', [ServiceController::class, 'index'])->name('services.index');
-    Route::get('/services/{id}/edit', [ServiceController::class, 'show'])->name('services.edit');
-    Route::get('/services/create', [ServiceController::class, 'create'])->name('services.create');
-    Route::get('/services/filter', [ServiceController::class, 'filter']);
-    Route::post('/services/store', [ServiceController::class, 'store'])->name('services.store');
-    Route::delete('/services/{id}', [ServiceController::class, 'destroy'])->name('services.destroy');
-    Route::put('/services/{id}', [ServiceController::class, 'update'])->name('services.update');
-    Route::put('/services/{id}/status', [ServiceController::class, 'updateStatus'])->name('services.updateStatus');
-
     //invoices
     Route::get('/invoices', [InvoiceController::class, 'index'])->name('invoices.index');
     Route::get('/invoices/filter', [InvoiceController::class, 'filter']);
