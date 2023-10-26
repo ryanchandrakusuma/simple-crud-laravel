96bb083a26a607daed2b3e1489d850e1cb20c587
Ryan Chandra Kusuma <rookie3ryan@gmail.com>
2023-10-26 13:56:40 +0700

    migration : base tables

--- database/migrations/2023_10_25_105933_create_taxes_table.php
+++ database/migrations/2023_10_25_105933_create_taxes_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('taxes', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100);
+            $table->integer('percent')->unsigned();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
--- database/migrations/2023_10_25_105943_create_products_table.php
+++ database/migrations/2023_10_25_105943_create_products_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name', 255);
-            $table->integer('stock');
             $table->timestamps();
             $table->softDeletes();
         });
